388dd536e5b9dc168c354661a78a051c83b34869
José Salinas <jzsalinas@gmail.com>
2026-08-01 16:18:19 -0300

    update version for Play Store.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "py.com.nugon"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
